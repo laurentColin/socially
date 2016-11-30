99e114fbe5baa4f9728a8ecb60a5d894a185dcdb
B@mbi_furtif <laurent.colin44@gmail.com>
2016-11-30 20:19:49 +0100

    upload base appli - chap 10

---
+++ client/main.js
@@ -0,0 +1,5 @@
+import angular from 'angular';
+import angularMeteor from 'angular-meteor';
+//import { Parties } from '../collections/parties';
+
+import { name as Socially } from '../imports/ui/components/socially/socially';
